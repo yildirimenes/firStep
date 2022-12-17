d06d773f120c0cae32ece5589ea1e40ff6da07c5
yildirimenes <104741427+yildirimenes@users.noreply.github.com>
2022-12-17 14:52:14 +0300

    first commitss

---
+++ lib/page2.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class page2 extends StatefulWidget {
+  const page2({Key? key}) : super(key: key);
+
+  @override
+  State<page2> createState() => _page2State();
+}
+
+class _page2State extends State<page2> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
